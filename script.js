007b4120fa30de83b91ffa9233f3c8c69c82620a
MarkGrayson <mgrayson15.6155000@outlook.com>
2026-01-20 15:34:33 +0000

    Implemented basic functionality to test movement buttons

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-const btnDown = document.querySelector("btn__start");
+const btnStart = document.querySelector("btn__start");
+const btnUp = document.querySelector("btn__up");
+const btnDown = document.querySelector("btn__down");
+const btnRight = document.querySelector("btn__right");
+const btnLeft = document.querySelector("btn__left");
 
+btnStart.addEventListener();
+btnUp.addEventListener();
 btnDown.addEventListener();
+btnRight.addEventListener();
+btnLeft.addEventListener();
 
 function startGame() {
 	alert("start button works");
+}
+
+function moveTilesDown() {
+	alert("down button works");
+}
+
+function moveTilesUp() {
+	alert("up button works");
+}
+
+function moveTilesRight() {
+	alert("right button works");
+}
+function moveTilesLeft() {
+	alert("left button works");
 }
