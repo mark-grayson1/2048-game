bb6ba18523d4be3c2585ecc5af7c49ec95dc855a
MarkGrayson <mgrayson15.6155000@outlook.com>
2026-01-22 16:09:34 +0000

    Completed functionality to reset grid and add 2 random squares

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const btnRight = document.querySelector("btn__right");
 const btnLeft = document.querySelector("btn__left");
 
 //create grid with all tiles initialized to 0 - no tiles
-const gridTracker = [
+var gridTracker = [
 	[0, 0, 0, 0],
-	[0, 0, 0, 0],
+	[0, 1, 0, 0],
 	[0, 0, 0, 0],
 	[0, 0, 0, 0],
 ];
@@ -23,7 +23,8 @@ const reset = () => {
 	score = 0;
 	playing = true;
 	score.textContent = "Score: 0";
-	console.log("Game reset");
+	resetGrid();
+	AddTile(2);
 };
 
 function moveTilesDown() {
@@ -39,4 +40,62 @@ function moveTilesRight() {
 }
 function moveTilesLeft() {
 	alert("left button works");
+}
+
+// Returns -1 if grid full else returns vacant
+function fillRandomVacantPosition() {
+	let emptySpaceCount = 0;
+	let result = -1; //Grid full
+
+	//How many empty spaces
+	for (let row = 0; row < gridTracker.length; row++) {
+		for (let column = 0; column < gridTracker.length; column++) {
+			if (gridTracker[row][column] === 0) emptySpaceCount++;
+		}
+	}
+
+	// Check if the grid is full
+	if (emptySpaceCount < 1) {
+		return -1;
+	}
+
+	// Randomly generate position between 1 and count
+	let randomPos = Math.floor(Math.random() * emptySpaceCount) + 1;
+
+	//Randomly generate 2 or 4
+	let evenNum = Math.floor(Math.random() * 2) + 1;
+	let tileNum = evenNum === 1 ? 2 : 4;
+
+	let currentPos = 0;
+	//Iterate to randomly selected empty square
+	for (let row = 0; row < gridTracker.length; row++) {
+		for (let column = 0; column < gridTracker.length; column++) {
+			if (gridTracker[row][column] === 0) {
+				if (currentPos === randomPos) {
+					gridTracker[row][column] = tileNum;
+					return randomPos;
+				}
+				currentPos++;
+			}
+		}
+	}
+}
+
+//CHECK IF SIMPLER WAY TO RESET THE GRID
+function resetGrid() {
+	for (let row = 0; row < gridTracker.length; row++) {
+		for (let column = 0; column < gridTracker.length; column++) {
+			gridTracker[row][column] = 0;
+		}
+	}
+}
+
+function updateGridDisplay() {}
+
+// Adds a randomly generated tile of 2 or 4
+function AddTile(quantity) {
+	for (let index = 1; index <= quantity; index++) {
+		fillRandomVacantPosition();
+		updateGridDisplay();
+	}
 }
